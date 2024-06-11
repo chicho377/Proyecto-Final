bb91c315d01667b5962cd047a3749f0ce2537e7d
chicho377 <morafabricio86@gmail.com>
2024-06-11 13:10:25 -0600

    Arreglando incrementacion de facturas

--- Ejercicio B/Codigo/VentaEntradasTeatro.cpp
+++ Ejercicio B/Codigo/VentaEntradasTeatro.cpp
@@ -14,6 +14,7 @@ int main(int argc, char** argv) {
     char continuar;
     
     do{
+    	numeroFactura++;
     	// ingresar datos del cliente
         printf("Ingrese cedula: ");
         scanf("%d", &cedula);
