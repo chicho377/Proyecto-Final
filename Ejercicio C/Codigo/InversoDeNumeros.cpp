d643d6ba7f036b5177a407bc91976f40ceddc421
chicho377 <morafabricio86@gmail.com>
2024-06-11 10:46:49 -0600

    Agregando validacion de que el numero tenga maximo 5 digitos

--- Ejercicio C/Codigo/InversoDeNumeros.cpp
+++ Ejercicio C/Codigo/InversoDeNumeros.cpp
@@ -8,9 +8,17 @@ int main(int argc, char** argv) {
 	char continuar;
 	
 	do{
+		// solicitar el numero al usuario
 		printf("Ingresa un numero entero (maximo 5 digitos): ");
 		scanf("%d", &numero);
 		
+		//validar que el numero tenga maximo 5 digitos
+		while(numero > 99999 || numero < 0){
+			printf("Numero invalido. Por favor ingrese un numero entre 0 y 99999: ");
+			scanf("%d", &numero);
+		}
+		
+		//preguntar si desea continuar
 		printf("Desea ingresar otro numero? (s/n): ");
 		scanf("%c", &continuar);
 	}while(continuar == 's' || continuar == 'S');
